d7b45296b2253c6b2000921edbc19a4260b99a5a
patilnitin16 <patilnitin7353@gmail.com>
2025-08-20 19:00:55 +0530

    file 2.1

---
+++ day_02_2.py
@@ -0,0 +1,36 @@
+'''
+We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
+
+Let's try to understand this with an example.
+
+You are given an immutable string, and you want to make changes to it.
+
+Example
+
+>>> string = "abracadabra"
+You can access an index by:
+
+>>> print string[5]
+a
+What if you would like to assign a value?
+
+>>> string[5] = 'k' 
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'str' object does not support item assignment
+How would you approach this?
+
+'''
+
+
+# Code
+
+def mutate_string(string, position, character):
+    newStr = string[:position] + character + string[position+1:]
+    return newStr
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
