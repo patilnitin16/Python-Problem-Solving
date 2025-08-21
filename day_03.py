2026dbd3d257b4ecf40f47abf38fecea3f33e87d
patilnitin16 <patilnitin7353@gmail.com>
2025-08-21 18:42:30 +0530

    day_03

---
+++ day_03.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        character = string[i]
+        for j in range(i+1,len(string)):
+            character+=string[j]
+            if character == sub_string:
+                count+=1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
