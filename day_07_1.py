d7d5e01d1a0d71829329e45320988189b2d12222
patilnitin16 <patilnitin7353@gmail.com>
2025-09-01 10:05:27 +0530

    day_07_2

--- day_07_1.py
+++ day_07_1.py
@@ -1,24 +1,11 @@
 '''
 You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
-
 For Example:
-
 Www.HackerRank.com → wWW.hACKERrANK.COM
 Pythonist 2 → pYTHONIST 2  
 Function Description
 
-Complete the swap_case function in the editor below.
-
-swap_case has the following parameters:
-
-string s: the string to modify
-Returns
-
-string: the modified string
-
 '''
-
-
 
 #Code
 
@@ -33,8 +20,6 @@ if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
-
-
 
 
 #BruteForce Method
--- day_07_2.py
+++ day_07_2.py
@@ -1,5 +1,19 @@
 ''''
+You have a non-empty set , and you have to execute  commands given in  lines.
 
+The commands will be pop, remove and discard.
+
+Input Format
+
+The first line contains integer , the number of elements in the set .
+The second line contains  space separated elements of set . All of the elements are non-negative integers, less than or equal to 9.
+The third line contains integer , the number of commands.
+The next  lines contains either pop, remove and/or discard commands followed by their associated value.
+
+
+Output Format
+
+Print the sum of the elements of set  on a single line.
 
 '''
 
@@ -7,3 +21,27 @@
 
 #Code
 
+#Optimized Method
+n = int(input())
+s = set(map(int, input().split()))
+N = int(input())
+for i in range(N):
+    cmd, *args = input().split()
+    getattr(s,cmd)(*map(int,args))
+
+
+
+
+#BruteForce Method
+n = int(input())
+s = set(map(int, input().split()))
+N = int(input())
+for i in range(N):
+    a = input().split(" ")
+    if a[0] == "pop":
+        s.pop()
+    elif a[0] == "remove":
+        s.remove(int(a[1]))
+    elif a[0] == "discard":
+        s.discard(int(a[1]))
+print(sum(s))
