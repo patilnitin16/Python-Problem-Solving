ae24e2c7e5292328d27be4a9837bbd3592ec3fb0
patilnitin16 <patilnitin7353@gmail.com>
2025-09-01 09:32:14 +0530

    day_07_1

--- day_07_1.py
+++ day_07_1.py
@@ -22,6 +22,22 @@ string: the modified string
 
 #Code
 
+#Optimized MEthod
+def swap_case(s):
+    return "".join(
+        letter.upper() if letter.islower() else letter.lower() 
+        for letter in s
+    )
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+
+
+
+#BruteForce Method
 def swap_case(s):
     newStr = ""
     for letter in s:
