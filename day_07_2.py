6ab472bc4886852ac6113ab26f001c96e09b2bdf
patilnitin16 <patilnitin7353@gmail.com>
2025-09-05 20:55:44 +0530

    day_08

--- day_07_2.py
+++ day_07_2.py
@@ -1,24 +1,12 @@
-''''
-You have a non-empty set , and you have to execute  commands given in  lines.
-
+''''You have a non-empty set , and you have to execute  commands given in  lines.
 The commands will be pop, remove and discard.
-
 Input Format
-
 The first line contains integer , the number of elements in the set .
 The second line contains  space separated elements of set . All of the elements are non-negative integers, less than or equal to 9.
 The third line contains integer , the number of commands.
 The next  lines contains either pop, remove and/or discard commands followed by their associated value.
-
-
 Output Format
-
-Print the sum of the elements of set  on a single line.
-
-'''
-
-
-
+Print the sum of the elements of set  on a single line.'''
 #Code
 
 #Optimized Method
