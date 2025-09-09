8a793599b97bc07d6dc3bf7cd9d83629221f3d39
patilnitin16 <patilnitin7353@gmail.com>
2025-09-09 22:59:50 +0530

    day_12

---
+++ day_12_1.py
@@ -0,0 +1,17 @@
+'''
+Task
+Read four numbers,a, b, c and d, and print the result of pow(a,b)+pow(c,d).
+
+Input Format
+Integers a, b, c, d and  are given on four separate lines, respectively.
+
+'''
+
+#Code
+
+#Optimized Code
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(pow(a,b)+pow(c,d))
