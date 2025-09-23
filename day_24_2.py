20d8e5287bdd90df01353c3ad7b36091d852a5e3
patilnitin16 <patilnitin7353@gmail.com>
2025-09-23 10:19:40 +0530

    day_24

---
+++ day_24_2.py
@@ -0,0 +1,31 @@
+'''
+Task
+You are given a string s.
+Your task is to find the indices of the start and end of string s in k.
+
+Input Format
+
+The first line contains the string s.
+The second line contains the string k.
+
+Output Format
+
+Print the tuple in this format: (start _index, end _index).
+If no match is found, print (-1, -1).
+
+'''
+
+#Code
+
+#Optimized Code
+
+import re
+s = input()
+k = input()
+pattern = f"(?=({k}))"
+matches = list(re.finditer(pattern,s))
+if matches:
+    for m in matches: 
+        print((m.start(),(m.start() + len(k) - 1)))
+else:
+    print((-1,-1))
