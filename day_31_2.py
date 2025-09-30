deb510afe817b7d388ca96723e816c1502cf0070
patilnitin16 <patilnitin7353@gmail.com>
2025-09-30 08:26:26 +0530

    day_31

---
+++ day_31_2.py
@@ -0,0 +1,22 @@
+'''
+Task
+
+You are given the shape of the array in the form of space-separated integers, each integer representing the size of different dimensions, your task is to print an array of the given shape and integer type using the tools numpy.zeros and numpy.ones.
+
+Input Format
+
+A single line containing the space-separated integers.
+
+Output Format
+
+First, print the array using the numpy.zeros tool and then print the array with the numpy.ones tool.
+'''
+
+#Code
+
+#Optimized Code
+import numpy
+
+N = tuple(map(int,input().split()))
+print(numpy.zeros((N),dtype = int))
+print(numpy.ones((N),dtype = int))
